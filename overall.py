95c6b4094c1371fa93b3cc7827aa8b8076e27aa2
Zac Robertson <zac.robertson-ctr@digitalreasoning.com>
2017-12-13 21:41:20 -0500

    overall results updated

--- overall.py
+++ overall.py
@@ -3,14 +3,16 @@ import sys
 import os
 
 def createTable():
-	df = pd.read_csv('logs/logs.csv')
+	df = pd.read_csv('Documents/ml_products/MLP_FinalProject/logs/logs.csv')
 	df = df.div(df.sum(axis=1), axis=0).sum() #normalize the rows then sum
 	df = df.T
 	df = df.sort_values()
+	df = pd.DataFrame(df, columns=['distance'])
+	df['Breed'] = df.index
 
 	return df
 
 if __name__ == '__main__':
-	#ratings = pd.read_csv(all_ratings_path)
+	ratings = pd.read_csv(all_ratings_path)
 	#ratings.set_index('Breed',inplace=True)
 	sys.exit(createTable(ratings))
