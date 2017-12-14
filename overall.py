af48829873bc868eec48661566a2df2b657ca2fc
Zac Robertson <zac.robertson-ctr@digitalreasoning.com>
2017-12-13 21:33:14 -0500

    overall results

---
+++ overall.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import sys
+import os
+
+def createTable():
+	df = pd.read_csv('logs/logs.csv')
+	df = df.div(df.sum(axis=1), axis=0).sum() #normalize the rows then sum
+	df = df.T
+	df = df.sort_values()
+
+	return df
+
+if __name__ == '__main__':
+	#ratings = pd.read_csv(all_ratings_path)
+	#ratings.set_index('Breed',inplace=True)
+	sys.exit(createTable(ratings))
